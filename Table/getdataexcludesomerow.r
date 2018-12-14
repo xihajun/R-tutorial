9993fc9624e9a57f6e23d5f1295559194f3dcc0d
xihajun <www.junfan.com@gmail.com>
2018-12-15 04:16:27 +0800

    Create getdataexcludesomerow.r

---
+++ Table/getdataexcludesomerow.r
@@ -0,0 +1,4 @@
+# Use -
+# Runs Grubbs' test excluding row 156
+grubbs.test(river$nitrate[-156])
+# From: https://campus.datacamp.com/courses/anomaly-detection-in-r/statistical-outlier-detection?ex=7
